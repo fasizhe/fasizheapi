d60fba0dbbe9df8d5dcae692f132e8c647e92350
linyongwei <5292972+linyongwei@user.noreply.gitee.com>
2019-10-19 17:02:15 +0800

    测试一下能不能提交code by lyw

--- src/main/java/com/faishze/api/fasizheapi/FasizheApiApplication.java
+++ src/main/java/com/faishze/api/fasizheapi/FasizheApiApplication.java
@@ -1,21 +1,29 @@
 package com.faishze.api.fasizheapi;
 
+import com.faishze.api.fasizheapi.dao.UserMapper;
+import com.faishze.api.fasizheapi.pojo.do0.User;
+import com.faishze.api.fasizheapi.validator.annotation.Phone;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication(scanBasePackages = "com")
 @RestController
 public class FasizheApiApplication {
+    @Autowired
+    private UserMapper userMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(FasizheApiApplication.class, args);
     }
     @RequestMapping("/")
-    public String index(){
-        return "hello";
+    public List<User> index(){
+        return userMapper.selectAll();
     }
 
 }
