a45190383eb9cb067c2a64578592e888513435bb
linyongwei <5292972+linyongwei@user.noreply.gitee.com>
2019-10-19 16:44:05 +0800

    测试一下能不能提交code by lyw

--- src/main/java/com/faishze/api/fasizheapi/FasizheApiApplication.java
+++ src/main/java/com/faishze/api/fasizheapi/FasizheApiApplication.java
@@ -1,13 +1,21 @@
 package com.faishze.api.fasizheapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@RestController
 public class FasizheApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FasizheApiApplication.class, args);
+    }
+    @RequestMapping("/")
+    public String index(){
+        return "hello1";
     }
 
 }
